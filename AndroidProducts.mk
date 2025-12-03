2e417e9c6a26148e636ae93886f60da464ccebf6
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-04 00:38:38 +0700

    Adding extra lunch option for DerpFest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -10,4 +10,5 @@ PRODUCT_MAKEFILES += \
 COMMON_LUNCH_CHOICES := \
     lineage_flashlmdd-user \
     lineage_flashlmdd-userdebug \
-    lineage_flashlmdd-eng
+    lineage_flashlmdd-eng \
+    lineage_flashlmdd-bp1a-user
