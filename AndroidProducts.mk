882e00ffde83d61f80edb054dfdbfd43e6da7e63
DunggVN <65992011+dunggvn@users.noreply.github.com>
2025-12-04 00:22:22 +0700

    Adding lunch choice (2)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,4 +8,6 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage_flashlmdd.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_flashlmdd-bp1a-user
+    lineage_flashlmdd-user \
+    lineage_flashlmdd-userdebug \
+    lineage_flashlmdd-eng
